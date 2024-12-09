2fbe11b3bfe8722dfbe5291b61f025b2090e38da
Hoyoung Yi <eric250@naver.com>
2024-12-09 17:28:09 +0900

    fix(MainPage): remove dummy push ui

--- src/app/pages/MainPage/index.tsx
+++ src/app/pages/MainPage/index.tsx
@@ -318,40 +318,6 @@ export function MainPage() {
         }}
       >
         <Map id="map" />
-        <div
-          style={{
-            alignSelf: 'center',
-            display: 'flex',
-            justifyContent: 'center',
-            position: 'absolute',
-            top: 'calc(env(safe-area-inset-top) + 16px)',
-            width: 'calc(100% - 2 * 16px)',
-            padding: '16px 32px',
-            backgroundColor: '#ffc0c0',
-            borderRadius: '16px',
-            boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.16)',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              columnGap: '4px',
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#12052e"
-            >
-              <path d="m40-120 440-760 440 760H40Zm138-80h604L480-720 178-200Zm302-40q17 0 28.5-11.5T520-280q0-17-11.5-28.5T480-320q-17 0-28.5 11.5T440-280q0 17 11.5 28.5T480-240Zm-40-120h80v-200h-80v200Zm40-100Z" />
-            </svg>
-            <div>불량 주차된 킥보드가 감지되었습니다</div>
-          </div>
-        </div>
         {/* <div
           style={{
             display: 'flex',
