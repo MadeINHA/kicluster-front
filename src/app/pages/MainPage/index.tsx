8c228332b5d3bdf946b0cd3766911060682bcd6b
Hoyoung Yi <eric250@naver.com>
2024-12-15 17:06:30 +0900

    feature(MainPage): add LoadingScreen, TowSuccessScreen, and Notification

--- src/app/pages/MainPage/index.tsx
+++ src/app/pages/MainPage/index.tsx
@@ -7,8 +7,11 @@ import useData from 'hooks/useData';
 import useRecommendedAreas from 'hooks/useRecommendedAreas';
 import useProhibitedAreas from 'hooks/useProhibitedAreas';
 import useExistingAreas from 'hooks/useExistingAreas';
+import Notification from 'app/components/Notification';
 import useKickBoards from 'hooks/useKickBoards';
 import useRedKickBoards from 'hooks/useRedKickBoards';
+import LoadingScreen from 'app/components/LoadingScreen';
+import TowSuccessScreen from 'app/components/TowSuccessScreen';
 
 // const UPDATE_TIME_INTERVAL = 30000;
 
@@ -16,6 +19,8 @@ export function MainPage() {
   const { getDynamicData } = useData();
 
   const [isKickBoardVisible, setIsKickBoardVisible] = useState(true);
+  const [isNotificationVisible, setIsNotificationVisible] = useState(false);
+  const [isLoadingScreenVisible, setIsLoadingScreenVisible] = useState(false);
   const [isRedKickBoardVisible, setIsRedKickBoardVisible] = useState(true);
 
   const { mapRef, goToMyLocation } = useMap();
@@ -24,6 +29,10 @@ export function MainPage() {
   useExistingAreas(mapRef);
   useKickBoards(mapRef, isKickBoardVisible);
   useRedKickBoards(mapRef, isRedKickBoardVisible);
+  const readyForTow = () => {
+    // setIsOnlyTowableKickBoardVisible(true);
+    // setIsNotificationVisible(false);
+  };
 
   return (
     <>
@@ -37,100 +46,17 @@ export function MainPage() {
           flexDirection: 'column',
           position: 'relative',
           height: '100%',
+          overflow: 'hidden',
         }}
       >
         <Map id="map" />
-        {/* <div
-          style={{
-            display: 'flex',
-            position: 'absolute',
-            top: 'env(safe-area-inset-top)',
-            left: 'env(safe-area-inset-left)',
-            padding: '24px',
-          }}
-        >
-          <TopButton
-            onClick={() => {
-              setIsKickBoardVisible(isKickBoardVisible => !isKickBoardVisible);
-            }}
-            style={{
-              backgroundColor: isKickBoardVisible ? '#6a26ff' : undefined,
-              color: isKickBoardVisible ? '#ffffff' : undefined,
-            }}
-          >
-            킥보드
-          </TopButton>
-        </div> */}
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            rowGap: '12px',
-            position: 'absolute',
-            bottom: 'calc(99px + env(safe-area-inset-bottom))',
-            right: 'env(safe-area-inset-right)',
-            padding: '24px',
-          }}
-        >
-          <IconButton
-            whileTap={{ scale: 0.95, backgroundColor: '#6a26ff' }}
-            transition={{ type: 'spring', stiffness: 400, damping: 30 }}
-            onClick={() => {
-              getDynamicData();
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-            >
-              <path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z" />
-            </svg>
-          </IconButton>
-          <IconButton
-            whileTap={{ scale: 0.95, backgroundColor: '#6a26ff' }}
-            transition={{ type: 'spring', stiffness: 400, damping: 30 }}
-            onClick={() => {
-              goToMyLocation();
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-            >
-              <path d="M440-42v-80q-125-14-214.5-103.5T122-440H42v-80h80q14-125 103.5-214.5T440-838v-80h80v80q125 14 214.5 103.5T838-520h80v80h-80q-14 125-103.5 214.5T520-122v80h-80Zm40-158q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-120q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Z" />
-            </svg>
-          </IconButton>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            columnGap: '12px',
-            position: 'absolute',
-            bottom: 'env(safe-area-inset-bottom)',
-            width: '100%',
-            padding: '24px',
-            backgroundColor: '#ffffff',
-            borderRadius: '12px 12px 0 0',
-            boxShadow: '0px 1px 4px 0px rgba(0, 0, 0, 0.16)',
-          }}
-        >
-          <BottomButton
-            whileTap={{ scale: 0.95, backgroundColor: '#6a26ff' }}
-            transition={{ type: 'spring', stiffness: 400, damping: 30 }}
-          >
-            주행 모드
-          </BottomButton>
-          <BottomButton
-            whileTap={{ scale: 0.95, backgroundColor: '#6a26ff' }}
-            transition={{ type: 'spring', stiffness: 400, damping: 30 }}
-          >
-            견인 모드
-          </BottomButton>
-        </div>
+        <LoadingScreen isVisible={isLoadingScreenVisible} />
+        <TowSuccessScreen />
+        <Notification
+          onClick={readyForTow}
+          isVisible={isNotificationVisible}
+          message="불량 주차된 킥보드가 감지되었습니다"
+        />
       </div>
     </>
   );
