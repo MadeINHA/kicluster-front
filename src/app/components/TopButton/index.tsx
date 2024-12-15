e64b70512600ff38dcde801fe73e6c9c2888b82a
Hoyoung Yi <eric250@naver.com>
2024-12-16 04:32:16 +0900

    feature(components): add TopButton

---
+++ src/app/components/TopButton/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components/macro';
+
+const TopButton = styled.div`
+  padding: 4px 16px;
+  background-color: #ffffff;
+  border-radius: 18px;
+  box-shadow: 0px 5px 7.5px 0px rgba(0, 0, 0, 0.2);
+  color: #515151;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+export default TopButton;
